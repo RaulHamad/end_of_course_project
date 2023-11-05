a085e59cff8a8dfbb9d360dc215e56ab3cd4a164
unknown <raulhamad@hotmail.com>
2023-11-05 05:01:48 +0100

    atualização 2

--- app.py
+++ app.py
@@ -1,23 +1,58 @@
-from flask import Flask, render_template,url_for, request,redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template,url_for, request,redirect,flash
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField,RadioField
+
+
+
 from models import *
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../database/luxury.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../database/luxurywheels.db'
+app.config["SECRET_KEY"] = '123456'
 db = SQLAlchemy()
+bcrypt = Bcrypt(app)
 db.init_app(app)
 
 
+class Formregister(FlaskForm):
+
+    username = StringField()
+    user_email = StringField()
+    user_password = PasswordField()
+    user_check_password = PasswordField()
+    user_category = RadioField(choices=[('value_1','Gold'),('value_2','Silver'),('value_3','Economy')],default='value_1')
+    submit = SubmitField()
 
 
 @app.route("/")
 @app.route("/index")
-def index():
+def page_index():
+
     return render_template("index.html")
 
-@app.route("/register")
-def register():
-    return render_template("register.html")
+@app.route("/register", methods=["GET","POST"])
+def page_register():
+    form = Formregister()
+    if form.validate_on_submit():
+
+        user1 = User(
+            name = form.username.data,
+            email = form.user_email.data,
+            password = form.user_password.data,
+
+        )
+        db.session.add(user1)
+        db.session.commit()
+
+        return redirect(url_for('page_index'))
+
+    return render_template("register.html", form=form)
+
+
+
+
 
 
 
--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from flask_sqlalchemy import SQLAlchemy
+
 
 from app import db,app
 
@@ -11,7 +11,7 @@ class Category(db.Model):
 
     categoria : Gold, Silver, Economic
     """
-    __tablename__ = "category_clients"
+    __tablename__ = "client_categories"
 
     id = db.Column(db.Integer, primary_key=True)
     category = db.Column(db.String(50),nullable=False, unique=True)
@@ -34,19 +34,19 @@ class User(db.Model):
     name = db.Column(db.String(50),nullable=False)
     email = db.Column(db.String(100),nullable=False, unique=True)
     password = db.Column(db.String(100),nullable=False)
-    category_id = db.Column(db.Integer, db.ForeignKey('category_clients.id'), nullable=False)
-    vehicles = db.relationship('Vehicle', backref='category_id', lazy=True)
+    client_categories_id = db.Column(db.Integer, db.ForeignKey('client_categories.id'), nullable=False)
+    vehicles = db.relationship('Vehicle', backref='client_categories_id', lazy=True)
     rent = db.relationship('Rent', backref='id', lazy=True)
 
 class Type(db.Model):
     """
          Criação da tabela Type para cadastro dos tipos de veiculos
-
+            tipo: Carro, moto
          id do cliente
          tipo do veiculo
 
          """
-    __tablename__ = "type_vehicle"
+    __tablename__ = "vehicle_types"
 
     id = db.Column(db.Integer, primary_key=True)
     type = db.Column(db.String(50),nullable=False, unique=True)
@@ -65,29 +65,36 @@ class Vehicle(db.Model):
     __tablename__ = "vehicles"
 
     id = db.Column(db.Integer, primary_key=True)
-    id_type = db.Column(db.String(50),db.ForeignKey("type_vehicle.id"),nullable=False)
+    type_id = db.Column(db.Integer,db.ForeignKey("vehicle_types.id"),nullable=False)
     color = db.Column(db.String(100),nullable=False)
     place = db.Column(db.Integer,nullable=False)
     service = db.Column(db.Integer, nullable=False)
     iva = db.Column(db.Boolean, nullable=False)
-    category = db.Column(db.Integer,db.ForeignKey("clients.category_id"), nullable=False)
+    category = db.Column(db.Integer,db.ForeignKey("clients.client_categories_id"), nullable=False)
     rent = db.relationship('Rent', backref='id', lazy=True)
 
 class Rent(db.Model):
 
-    __tablename__ = "rent"
+    __tablename__ = "rents"
 
-    id_rent = db.Column(db.Integer,primary_key=True)
-    id_client = db.Column(db.Integer,db.ForeignKey("clients.id"), nullable=False)
-    id_vehicle = db.column(db.Integer,db.ForeignKey("vehicles.id"))
+    rent_id = db.Column(db.Integer,primary_key=True)
+    client_id = db.Column(db.Integer,db.ForeignKey("clients.id"), nullable=False)
+    vehicle_id = db.column(db.Integer,db.ForeignKey("vehicles.id"))
     pick_up_date = db.Column(db.Date, nullable=False)
     return_date = db.Column(db.Date, nullable=False)
     price_day = db.Column(db.Float,nullable=False)
     status_rent = db.Column(db.Boolean,nullable=False)
     total_price = db.Column(db.Float,nullable=False)
 
+
+
+
+
 with app.app_context():
+
+
     db.create_all()
     db.session.commit()
+
 
 
